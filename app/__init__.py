7eb4955d1b0bc23d204d3132ee92e0da368fae4a
ketw <34557433+ketw@users.noreply.github.com>
2025-11-23 08:54:56 +0530

    internal changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,23 @@
-
-from flask import Flask
+from .routes import main
 import os
+from flask import Flask
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    
+
     upload_dir = os.path.join(app.root_path, "uploads")
+    temp_dir = os.path.join(app.root_path, "temp")
     os.makedirs(upload_dir, exist_ok=True)
-    app.config["UPLOAD_FOLDER"] = upload_dir
+    os.makedirs(temp_dir, exist_ok=True)
 
-    # Register routes
-    from .routes import main
+    app.config["directory_upload"] = upload_dir
+    app.config["directory_temp"] = temp_dir
+    app.config["max_content_length"] = 50 * 1024 * 1024
+
     app.register_blueprint(main)
-
     return app
+
+
 
 
