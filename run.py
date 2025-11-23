863a1e3c0de02df53d7e022ccfbc50b3d6202f32
Rijwan <Rijwan25072004@gmail.com>
2025-11-23 08:36:49 +0545

    changed ip address to global

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ app = create_app()
 if __name__ == "__main__":
     # Run with HTTPS using self-signed certs
     app.run(
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=433,
         # ssl_context=("certs/cert.pem", "certs/key.pem")
     )
